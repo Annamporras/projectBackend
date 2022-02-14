19459bcb931a649b7e4b1149d40a845c289fa983
Anna Ma Porras <amporras85@gmail.com>
2022-02-14 15:36:52 +0100

    cambios errores

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 module.exports = app => {
 
-  const authRouter = require("./auth.routes")
-  app.use("/", authRouter)
 
-  const userRouter = require('./user.routes')
-  app.use('/', userRouter)
+  app.use("/", require("./auth.routes"))
+
+  // const userRouter = require('./user.routes')
+  // app.use('/', userRouter)
 
 }
 
