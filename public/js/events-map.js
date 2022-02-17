b24c9b31a8f59edef20b7d9e11528edf337bc571
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-02-17 17:11:41 +0100

    funcia maapss

--- public/js/events-map.js
+++ public/js/events-map.js
@@ -1,35 +1,51 @@
+let map
 function initMap() {
-
     drawMap()
     printMarker()
-    //getPlaces()
-}
-
+ }
 
 function drawMap() {
-    const options = {
-        center: { lat: 40.41266973724484, lng: - 3.7106261089045036 },
-        zoom: 15
-    }
-    const mapInstance = new google.maps.Map(document.querySelector('#myMap'), options)
-}
-function printMarker() {
-    const { Marker } = google.maps
-    const inputs = document.querySelectorAll  //('.d-none input')  // pinta cordenadas trasnspartes
+    const {Map}=google.maps
+    const inputs = document.querySelectorAll(".d-none input")
 
     const latitude = Number(inputs[0].value)
     const longitude = Number(inputs[1].value)
 
-    new Marker({
-        map, // pintar la chincheta dentro del mapa centrado con las mismas coordenadas
-        position: { lat: latitude, lng: longitude }
-    })
+    map = new Map(
+        document.getElementById("myMap"),
+        {
+            zoom: 16,
+            center: {lat: latitude, lng: longitude}
+    
+        }
+    )
 }
-// const myMarker = new google.maps.Marker({
-//   position: {
-//   	lat: 41.3977381,
-//   	lng: 2.190471916
-//   },
-//   map: map,
-//   title: "I'm here"
-// });
+
+function printMarker() {
+    const {Marker}=google.maps
+    const inputs = document.querySelectorAll(".d-none input")
+
+    const latitude = Number(inputs[0].value)
+    const longitude = Number(inputs[1].value)
+
+    new Marker(
+        {
+            map,
+            position: { 
+                lat: latitude,
+                lng: longitude
+
+            }
+        }
+
+    )
+
+}
+
+
+
+
+
+
+
+
