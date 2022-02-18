38587213a18ed59740daa9defc2f90ac420e8aba
Anna Ma Porras <amporras85@gmail.com>
2022-02-18 11:08:30 +0100

    muerte favicon

--- config/index.js
+++ config/index.js
@@ -16,5 +16,4 @@ module.exports = (app) => {
   app.set("view engine", "hbs");
   app.use(express.static(path.join(__dirname, "..", "public")));
 
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
 };
